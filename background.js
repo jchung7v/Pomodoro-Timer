87e9d9ba2c5d11131f2e52bbe76ed2142dfb8001
Juan Chung <jcdev@Juans-MacBook-Pro.local>
2023-09-03 22:02:30 -0700

    Encountered the problem

--- background.js
+++ background.js
@@ -1,12 +1,3 @@
-// chrome.storage.local.get(["workTime"], function (result) {
-//   workTime = parseInt(result.workTime) || 0;
-// });
-// chrome.storage.local.get(["breakTime"], function (result) {
-//   breakTime = parseInt(result.breakTime) || 0;
-// });
-// chrome.storage.local.get(["sessions"], function (result) {
-//   sessions = parseInt(result.sessions) || 0;
-// });
 let workTime = 0;
 let breakTime = 0;
 let sessions = 0;
@@ -15,12 +6,26 @@ let paused = false;
 let elapsedTime = 0;
 let currentTime;
 let timeInSecs;
+let sessionActive = false;
 
-chrome.storage.local.get(["workTime", "breakTime", "sessions"], function (result) {
-  workTime = parseInt(result.workTime) || 0;
-  breakTime = parseInt(result.breakTime) || 0;
-  sessions = parseInt(result.sessions) || 0;
-});
+chrome.storage.local.get(
+  [
+    "workTime",
+    "breakTime",
+    "sessions",
+    "timeInSecs",
+    "sessionActive",
+    "sessionCounter",
+  ],
+  function (result) {
+    workTime = parseInt(result.workTime) || 0;
+    breakTime = parseInt(result.breakTime) || 0;
+    sessions = parseInt(result.sessions) || 0;
+    timeInSecs = parseInt(result.timeInSecs) || 0;
+    sessionActive = result.sessionActive || false;
+    sessionCounter = parseInt(result.sessionCounter) || 0;
+  }
+);
 
 // receive a message from popup.js and respond to it
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -34,14 +39,23 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     case "RESET_TIMER":
       resetTimer();
       break;
+    case "RESET_ALL":
+      resetAll();
+      break;
   }
 });
 
 // start new session
 async function startSession() {
+  if (sessionActive) return;
+  sessionActive = true;
+  chrome.storage.local.set({ sessionActive: true });
+
   console.log("startSession Activated!");
+
   for (let i = 1; i <= sessions; i++) {
     sessionCounter = i;
+    chrome.storage.local.set({ sessionCounter: sessionCounter });
     await startTimer(workTime);
     await startTimer(breakTime);
     chrome.runtime.sendMessage({
@@ -49,6 +63,8 @@ async function startSession() {
       sessionCounter: sessionCounter,
     });
   }
+  chrome.storage.local.set({ sessionActive: false });
+  sessionActive = false;
 }
 
 // start work/break timer
@@ -77,6 +93,7 @@ function startTimer(duration) {
           timeInSecs: timeInSecs,
           elapsedTime: elapsedTime,
         });
+        chrome.storage.local.set({ timeInSecs: timeInSecs });
       }
     }, 1000);
   });
@@ -94,4 +111,22 @@ function pauseTimer() {
 function resetTimer() {
   paused = false;
   startTimer(workTime, "Work Timer");
+}
+
+function resetAll() {
+  clearInterval(currentTime);
+  timeInSecs = 0;
+  sessionCounter = 0;
+  sessionActive = false;
+  paused = false;
+
+  chrome.storage.local.set({
+    workTime: 0,
+    breakTime: 0,
+    sessions: 0,
+    timeInSecs: 0,
+    sessionActive: false,
+    sessionCounter: 0,
+  });
+  chrome.runtime.sendMessage({ type: "ALL_RESET" });
 }
--- popup.js
+++ popup.js
@@ -1,23 +1,30 @@
+const popupPage = document.querySelector(".popup-page");
+const timerPage = document.querySelector(".timer-page");
+
 const workButtonsContainer = document.getElementById("work-btn-container");
 const breakButtonsContainer = document.getElementById("break-btn-container");
 const sessionButtonsContainer = document.getElementById(
   "session-btn-container"
 );
+const resetIcon = document.getElementById("reset-icon");
+const playPauseIcon = document.getElementById("play-pause-icon");
+const backButton = document.getElementById("back-icon");
 const GO = document.getElementById("go-btn");
+const resetAllButton = document.getElementById("reset-all");
 
 let workTime = 0;
 let breakTime = 0;
 let sessions = 0;
-// let workTimeInSecs;
-// let breakTimeInSecs;
-// let workTimer;
-// let isClicked = false;
+let sessionCounter = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let timeInSecs = 0;
 
-// store workTime, breakTime, and # of sessions in a local storage
+// Store workTime, breakTime, and number of sessions in a local storage
 workButtonsContainer.addEventListener("click", (e) => {
   if (e.target.dataset.work) {
     workTime = parseInt(e.target.dataset.work, 10);
-    chrome.storage.local.set({ workTime: workTime});
+    chrome.storage.local.set({ workTime: workTime });
     deselectAllButtons(workButtonsContainer);
     e.target.classList.remove("button-original-color");
     e.target.classList.add("button-clicked-color");
@@ -27,7 +34,7 @@ workButtonsContainer.addEventListener("click", (e) => {
 breakButtonsContainer.addEventListener("click", (e) => {
   if (e.target.dataset.break) {
     breakTime = parseInt(e.target.dataset.break, 10);
-    chrome.storage.local.set({ breakTime: breakTime});
+    chrome.storage.local.set({ breakTime: breakTime });
     deselectAllButtons(breakButtonsContainer);
     e.target.classList.remove("button-original-color");
     e.target.classList.add("button-clicked-color");
@@ -37,35 +44,134 @@ breakButtonsContainer.addEventListener("click", (e) => {
 sessionButtonsContainer.addEventListener("click", (e) => {
   if (e.target.dataset.session) {
     sessions = parseInt(e.target.dataset.session, 10);
-    chrome.storage.local.set({ sessions: sessions});
+    chrome.storage.local.set({ sessions: sessions });
     deselectAllButtons(sessionButtonsContainer);
     e.target.classList.remove("button-original-color");
     e.target.classList.add("button-clicked-color");
   }
 });
 
-// initiate a session if all three types of buttons are clicked.
+// Deselect a button when the other button is clicked
+function deselectAllButtons(container) {
+  container.querySelectorAll(".button").forEach((button) => {
+    button.classList.add("button-original-color");
+    button.classList.remove("button-clicked-color");
+  });
+}
+
+// Initiate a session
 GO.addEventListener("click", () => {
   if (workTime != 0 && breakTime != 0 && sessions != 0) {
-      chrome.runtime.sendMessage({
+    chrome.runtime.sendMessage({
       type: "SET_TIMER",
-      // workTime: workTime,
-      // breakTime: breakTime,
-      // sessions: sessions,
     });
-    window.location.href = "timer.html";
+    popupPage.style.display = "none";
+    timerPage.style.display = "block";
   } else {
     document.getElementById("notice").innerHTML =
       "Please press all three types of buttons";
   }
 });
 
-// deselect a button when the other button is clicked
-function deselectAllButtons(container) {
-  container.querySelectorAll("button").forEach((button) => {
-    button.classList.add("button-original-color");
-    button.classList.remove("button-clicked-color");
+//////////////////////////////////////////////
+
+// fetch timer from service worker ever second
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log("TIME_IN_SECS received, sending back: ");
+  timeInSecs = message.timeInSecs;
+  let mins = Math.floor(timeInSecs / 60);
+  let secs = timeInSecs % 60;
+  let view =
+    (mins < 10 ? "0" : "") + mins + " : " + (secs < 10 ? "0" : "") + secs;
+  document.getElementById("countdown").innerHTML = view;
+});
+
+chrome.storage.local.get("timeInSecs", function (result) {
+  timeInSecs = parseInt(result.timeInSecs) || 0;
+  updateTimerDisplay();
+});
+
+function updateTimerDisplay() {
+  let mins = Math.floor(timeInSecs / 60);
+  let secs = timeInSecs % 60;
+  let view =
+    (mins < 10 ? "0" : "") + mins + " : " + (secs < 10 ? "0" : "") + secs;
+  document.getElementById("countdown").innerHTML = view;
+}
+
+playPauseIcon.addEventListener("click", () => {
+  chrome.runtime.sendMessage({
+    type: "PAUSE_TIMER",
+  });
+});
+
+resetIcon.addEventListener("click", () => {
+  chrome.runtime.sendMessage({
+    type: "RESET_TIMER",
+  });
+});
+
+backButton.addEventListener("click", () => {
+  clearInterval(currentTime);
+
+  popupPage.style.display = "block";
+  timerPage.style.display = "none";
+});
+
+resetAllButton.addEventListener("click", () => {
+  chrome.runtime.sendMessage({
+    type: "RESET_ALL",
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  chrome.storage.local.get(["sessionActive"], function (result) {
+    if (result.sessionActive) {
+      popupPage.style.display = "none";
+      timerPage.style.display = "block";
+
+      timeInSecs = result.timeInSecs || 0;
+      updateTimerDisplay();
+
+      sessionCounter = result.sessionCounter || 0;
+    }
+  });
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "PLAY_AUDIO") {
+    playAudio();
+  } else if (message.type === "ADD_STAR") {
+    if (message.sessionCounter >= 1 && message.sessionCounter <= 5) {
+      checkStar();
+    }
+  }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "ALL_RESET") {
+    popupPage.style.display = "block";
+    timerPage.style.display = "none";
+  }
+});
+
+function checkStar() {
+  console.log("Star Sticker is Activated!");
+  return new Promise((resolve) => {
+    let starElement = document.createElement("img");
+    starElement.src = "./images/star.jpg";
+    starElement.alt = "Star Sticker";
+    starElement.style.width = "50px";
+    document.getElementById("stickers").appendChild(starElement);
+    resolve();
   });
 }
 
-function sendUserSelectedTime() {}
+async function playAudio() {
+  const audio = new Audio("./images/promise-616.mp3");
+  try {
+    await audio.play();
+  } catch (err) {
+    console.log("You must allow audio play in the browser");
+  }
+}
